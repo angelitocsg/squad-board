9e31b0c13310309e689a6746f261411582517795
Angelito Casagrande <angelito@angelito.com.br>
2023-04-20 11:36:05 -0300

    Add todo

--- src/pages/projects/containers/SummaryActiveGmuds.tsx
+++ src/pages/projects/containers/SummaryActiveGmuds.tsx
@@ -9,6 +9,12 @@ const SummaryActiveGmuds = ({ gmuds }: IProps) => {
   return gmuds && gmuds.length > 0 ? (
     <>
       <h1 className="h4 pb-2">GMUDs ativas / previsão de publicação</h1>
+      <h2 className="h6">Todo:</h2>
+      <ul>
+        <li>Encerrar tarefa de validação durante a janela de publicação</li>
+        <li>Encerrar GMUD após a janela de publicação (dia seguinte)</li>
+        <li>Concluir merge para branch main após validação da publicação</li>
+      </ul>
       <div className="border bg-white p-3 mb-5">
         <PaneGmud
           gmuds={gmuds}
