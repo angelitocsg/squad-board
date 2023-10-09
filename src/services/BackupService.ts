5e1d47ffa549e77b19e239f5b36e1ccc370b7345
Angelito Casagrande <angelito@angelito.com.br>
2023-10-08 23:33:58 -0300

    feat: add backup service

---
+++ src/services/BackupService.ts
@@ -0,0 +1,44 @@
+import ExportHelper from "../helpers/export.helper";
+
+export class BackupService {
+  static importBackup() {
+    const fileInput = document.getElementById("fileInput") as HTMLInputElement;
+    fileInput?.click();
+    fileInput?.addEventListener("change", handleFiles, false);
+    function handleFiles() {
+      if (!fileInput.files || !fileInput.files[0]) return;
+      const file = fileInput.files[0];
+      const reader = new FileReader();
+      const encodingFromSettings =
+        localStorage.getItem("encoding") ??
+        "UTF-8" ??
+        "iso-8859-1" ??
+        "windows-1252";
+      reader.onabort = () => console.error("file reading was aborted");
+      reader.onerror = () => console.error("file reading has failed");
+      reader.onload = () => {
+        const data = JSON.parse((reader.result as string) ?? "[]");
+        const keys = Object.keys(data);
+        keys.forEach(key =>
+          localStorage.setItem(key, JSON.stringify(data[key])),
+        );
+      };
+      console.info("Reader encoding:", encodingFromSettings);
+      reader.readAsText(file, encodingFromSettings);
+    }
+  }
+
+  static exportBackup() {
+    const backupKeys = [];
+    let data = {};
+    for (let key in localStorage) {
+      if (!key.startsWith("data_")) break;
+      backupKeys.push(key);
+      data = { ...data, [key]: JSON.parse(localStorage.getItem(key) ?? "{}") };
+    }
+    console.log(data);
+    let dt = new Date().toISOString();
+    dt = dt.replace(/-/g, "").replace(/:/g, "").substring(0, 13);
+    ExportHelper.jsonFile(data, `backup_${dt}`);
+  }
+}
--- src/shared/PageLayout/index.tsx
+++ src/shared/PageLayout/index.tsx
@@ -1,9 +1,8 @@
-import { StorageKey } from "../../enums/StorageKey";
-import ExportHelper from "../../helpers/export.helper";
 import AlertModal from "../../modules/core/components/AlertModal";
 import AppModal from "../../modules/core/components/AppModal";
 import FloatingButtonMenu from "../../modules/core/components/FloatingButtonMenu";
 import FormImport from "../../modules/core/components/FormImport";
+import { BackupService } from "../../services/BackupService";
 
 type IProps = {
   title?: string;
@@ -15,48 +14,13 @@ const floatingButtons = [
     title: "Importar",
     icon: "bi-upload",
     position: { fontSize: "1.3em" },
-    onClick: () => {
-      const fileInput = document.getElementById("fileInput") as HTMLInputElement;
-      fileInput?.click();
-      fileInput?.addEventListener("change", handleFiles, false);
-      function handleFiles() {
-        if (!fileInput.files || !fileInput.files[0]) return;
-        const file = fileInput.files[0];
-        const reader = new FileReader();
-        const encodingFromSettings =
-          localStorage.getItem("encoding") ?? "UTF-8" ?? "iso-8859-1" ?? "windows-1252";
-        reader.onabort = () => console.error("file reading was aborted");
-        reader.onerror = () => console.error("file reading has failed");
-        reader.onload = () => {
-          const data = JSON.parse((reader.result as string) ?? "[]");
-          const keys = Object.keys(data);
-          keys.forEach(key => localStorage.setItem(key, JSON.stringify(data[key])));
-        };
-        console.info("Reader encoding:", encodingFromSettings);
-        reader.readAsText(file, encodingFromSettings);
-      }
-    },
+    onClick: BackupService.importBackup,
   },
   {
     title: "Exportar",
     icon: "bi-download",
     position: { fontSize: "1.3em" },
-    onClick: () => {
-      let data = {
-        [StorageKey.DATA_GESTAO_MUDANCA]: JSON.parse(
-          localStorage.getItem(StorageKey.DATA_GESTAO_MUDANCA) ?? "{}",
-        ),
-        [StorageKey.DATA_PROD_DIGITAL]: JSON.parse(
-          localStorage.getItem(StorageKey.DATA_PROD_DIGITAL) ?? "{}",
-        ),
-        [StorageKey.DATA_REPOSITORIOS]: JSON.parse(
-          localStorage.getItem(StorageKey.DATA_REPOSITORIOS) ?? "{}",
-        ),
-      };
-      let dt = new Date().toISOString();
-      dt = dt.replace(/-/g, "").replace(/:/g, "").substring(0, 13);
-      ExportHelper.jsonFile(data, `backup_${dt}`);
-    },
+    onClick: BackupService.exportBackup,
   },
 ];
 
