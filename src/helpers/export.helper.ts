fc0f0494c9ec6fdac5c6e3e054c6a6d59bdb9de6
Angelito Casagrande <angelito@angelito.com.br>
2023-11-08 23:59:30 -0300

    fix: import export services

--- src/helpers/export.helper.ts
+++ src/helpers/export.helper.ts
@@ -1,5 +1,7 @@
 const jsonFile = (exportData: any, fileName: string) => {
-  const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportData));
+  const dataStr =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(exportData));
   const dlAnchorElem = document.createElement("a");
   dlAnchorElem?.setAttribute("href", dataStr);
   dlAnchorElem?.setAttribute("download", `${fileName}_${Date.now()}.json`);
@@ -8,7 +10,8 @@ const jsonFile = (exportData: any, fileName: string) => {
 };
 
 const csvFile = (exportData: any, fileName: string) => {
-  const dataStr = "data:text/csv;charset=utf-8," + encodeURIComponent(exportData);
+  const dataStr =
+    "data:text/csv;charset=utf-8," + encodeURIComponent(exportData);
   const dlAnchorElem = document.createElement("a");
   dlAnchorElem?.setAttribute("href", dataStr);
   dlAnchorElem?.setAttribute("download", `${fileName}_${Date.now()}.csv`);
--- src/services/BackupService.ts
+++ src/services/BackupService.ts
@@ -12,13 +12,19 @@ export class BackupService {
       if (!fileInput.files || !fileInput.files[0]) return;
       const file = fileInput.files[0];
       const reader = new FileReader();
-      const encodingFromSettings = localStorage.getItem("encoding") ?? "UTF-8" ?? "iso-8859-1" ?? "windows-1252";
+      const encodingFromSettings =
+        localStorage.getItem("encoding") ??
+        "UTF-8" ??
+        "iso-8859-1" ??
+        "windows-1252";
       reader.onabort = () => console.error("file reading was aborted");
       reader.onerror = () => console.error("file reading has failed");
       reader.onload = () => {
         const data = JSON.parse((reader.result as string) ?? "[]");
         const keys = Object.keys(data);
-        keys.forEach(key => localStorage.setItem(key, JSON.stringify(data[key])));
+        keys.forEach((key) =>
+          localStorage.setItem(key, JSON.stringify(data[key])),
+        );
       };
       console.info("Reader encoding:", encodingFromSettings);
       reader.readAsText(file, encodingFromSettings);
@@ -38,17 +44,27 @@ export class BackupService {
     ExportHelper.jsonFile(data, `backup_${dt}`);
   }
 
-  static exportDataAsCsv(data: any[]) {
+  static exportDataAsCsv(data: any[], fileName?: string) {
     if (!data || data.length === 0) return;
-
     const columns = Object.keys(data[0]);
+    console.log({ columns });
+
+    const fncLine = (line: any) =>
+      `${columns.map((col) => fncColumns(line, col)).join(";")}\n`;
+
+    const fncColumns = (line: any, col: string) => {
+      if (typeof line[col] === "string")
+        return `"${line[col]?.replace(/\t/g, "")}"`;
+      if (typeof line[col] === "boolean") return line[col];
+      return line[col] !== undefined ? `"${line[col]}"` : "";
+    };
+
     const content =
-      `${columns.join(";")}\n` +
-      data.map(line => `${columns.map(col => line[col].replace(/\t/g, "")).join(";")}\n`).join("");
+      `${columns.join(";")}\n` + data.map((line) => fncLine(line)).join("");
 
     let dt = new Date().toISOString();
     dt = dt.replace(/-/g, "").replace(/:/g, "").substring(0, 13);
-    ExportHelper.csvFile(content, `data_${dt}`);
+    ExportHelper.csvFile(content, `${fileName ?? "data"}_${dt}`);
   }
 
   static importCsvToData(key: string) {
@@ -60,7 +76,11 @@ export class BackupService {
       if (!fileInput.files || !fileInput.files[0]) return;
       const file = fileInput.files[0];
       const reader = new FileReader();
-      const encodingFromSettings = localStorage.getItem("encoding") ?? "UTF-8" ?? "iso-8859-1" ?? "windows-1252";
+      const encodingFromSettings =
+        localStorage.getItem("encoding") ??
+        "UTF-8" ??
+        "iso-8859-1" ??
+        "windows-1252";
       reader.onabort = () => console.error("file reading was aborted");
       reader.onerror = () => console.error("file reading has failed");
       reader.onload = () => {
@@ -69,14 +89,14 @@ export class BackupService {
         let columns = parsedCsv.data.shift() as string[];
         const lines = parsedCsv.data as [];
         const dtos = lines
-          .map(line => {
+          .map((line) => {
             const it: any = {};
             columns.forEach((col, i) => {
               it[col] = line[i];
             });
             return it;
           })
-          .filter(x => x.id);
+          .filter((x) => x.id);
         localStorage.setItem(key, JSON.stringify(dtos));
         fileInput?.removeEventListener("change", handleFiles, false);
         setTimeout(() => {
