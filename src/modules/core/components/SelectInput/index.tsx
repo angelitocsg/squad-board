f66d33d14850230d3651315e1d0d7911fea65706
Angelito Casagrande <angelito@angelito.com.br>
2024-04-18 23:22:21 -0300

    create checkbox select

--- src/modules/core/components/SelectInput/index.tsx
+++ src/modules/core/components/SelectInput/index.tsx
@@ -1,11 +1,14 @@
 import { ChangeEventHandler } from "react";
+import { TBadge } from "../DisplayTable/BadgeType";
 
 export type ISelectOptions = {
   value: any;
   label: string;
+  color?: TBadge;
 };
 
 interface ISelectInput {
+  variation?: "default" | "checkbox";
   label: string;
   field: string;
   value?: string | number | readonly string[];
@@ -16,7 +19,14 @@ interface ISelectInput {
   multiple?: boolean;
   onChange?: ChangeEventHandler<HTMLSelectElement>;
 }
-const SelectInput = ({
+const SelectInput = (props: ISelectInput) =>
+  props.variation === "checkbox" ? (
+    <SelectInputCheckbox {...props} />
+  ) : (
+    <SelectInputDefault {...props} />
+  );
+
+const SelectInputDefault = ({
   label,
   field,
   value,
@@ -48,6 +58,85 @@ const SelectInput = ({
         </option>
       ))}
     </select>
+  </div>
+);
+
+const Checkbox = ({
+  field,
+  label,
+  value,
+  color,
+  checked,
+  idx,
+  onChange,
+}: any) => (
+  <>
+    <input
+      key={`${field}${idx}`}
+      type="radio"
+      className="btn-check"
+      name={field}
+      id={`${field}${idx}`}
+      value={value}
+      autoComplete="off"
+      checked={checked}
+      onChange={onChange}
+    />
+    <label
+      className={`btn btn-outline-${
+        color ? color : "primary"
+      } text-dark border`}
+      htmlFor={`${field}${idx}`}>
+      {label}
+    </label>
+  </>
+);
+
+const SelectInputCheckbox = ({
+  label,
+  field,
+  value,
+  defaultValue,
+  disabled,
+  options,
+  onChange,
+}: ISelectInput) => (
+  <div className="mb-3">
+    <label htmlFor={field} className="form-label">
+      {label}
+    </label>
+    <div className="btn-group-vertical btn-group-sm d-grid gap-0" role="group">
+      {options.map((st, idx) => (
+        <Checkbox
+          key={idx}
+          field={field}
+          label={st.label}
+          value={st.value}
+          color={st.color}
+          checked={st.value === value}
+          idx={idx}
+          onChange={onChange}
+        />
+      ))}
+    </div>
+
+    {/* <select
+      className="form-select"
+      multiple={multiple}
+      id={field}
+      name={field}
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      placeholder={placeholder}
+      disabled={disabled}
+      title={`campo: ${field}`}>
+      {options.map((opt, i) => (
+        <option key={i} value={opt.value}>
+          {opt.label}
+        </option>
+      ))}
+    </select> */}
   </div>
 );
 
